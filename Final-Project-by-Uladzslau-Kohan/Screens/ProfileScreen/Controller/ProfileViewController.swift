73b8594c148d11b53ffa3e2d689cb225369be667
Volcheslav <vladkohanalex@gmail.com>
2022-08-23 14:29:56 +0300

    Custom button editor added to storyboard, hiding keyboard by tap or by pressing return added, added textfields for login and password

--- Final-Project-by-Uladzslau-Kohan/Screens/ProfileScreen/Controller/ProfileViewController.swift
+++ Final-Project-by-Uladzslau-Kohan/Screens/ProfileScreen/Controller/ProfileViewController.swift
@@ -7,15 +7,23 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
 
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loginTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.initializeHideKeyboard()
+        self.passwordTextField.placeholder = "Enter your password"
+        self.loginTextField.placeholder = "Enter your login"
+        self.loginTextField.keyboardType = .emailAddress
+        self.passwordTextField.isSecureTextEntry = true
+        self.passwordTextField.delegate = self
+        self.loginTextField.delegate = self
     }
-    
-
     /*
     // MARK: - Navigation
 
@@ -25,5 +33,26 @@ class ProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: Hiding keybord functions
 
+    private func initializeHideKeyboard() {
+        let tap = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissMyKeyboard)
+        )
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc private func dismissMyKeyboard() {
+        view.endEditing(true)
+    }
+
+}
+
+extension ProfileViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
---
+++ Final-Project-by-Uladzslau-Kohan/Screens/ProfileScreen/View/CustomButton.swift
@@ -0,0 +1,79 @@
+//
+//  CustomButton.swift
+//  Final-Project-by-Uladzslau-Kohan
+//
+//  Created by VironIT on 8/23/22.
+//
+//  swiftlint: disable all
+import Foundation
+import UIKit
+
+@IBDesignable
+class UICustomButton: UIButton {
+    // button border colors
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+           self.layer.borderColor = borderColor.cgColor
+        }
+    }// button border width
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+           self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    // button corner radius
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+           self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    public override class var layerClass: AnyClass {
+           CAGradientLayer.self
+    }
+        
+    private var gradientLayer: CAGradientLayer {
+            layer as! CAGradientLayer
+    }// button start color gradient
+    @IBInspectable public var startColor: UIColor = .white {
+        didSet {
+           updateColors()
+        }
+    }
+        
+    // button end color gradient
+    @IBInspectable public var endColor: UIColor = .red {
+       didSet {
+          updateColors()
+       }
+    }
+        
+    // button start point gradient
+    @IBInspectable public var startPoint: CGPoint {
+        get {
+            gradientLayer.startPoint
+        }
+            
+        set {
+            gradientLayer.startPoint = newValue
+        }
+     }
+        
+     // button end point gradient
+     @IBInspectable public var endPoint: CGPoint {
+        get {
+            gradientLayer.endPoint
+        }
+        set {
+            gradientLayer.endPoint = newValue
+        }
+     }
+        
+     // update colores gradient
+     private func updateColors() {
+            gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+     }
+}
+
+//swiftlint: enable all
