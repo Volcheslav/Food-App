bae8bf1254f39d824f7e676ae0ee899bcdb54559
Volcheslav <vladkohanalex@gmail.com>
2022-08-25 15:14:36 +0300

    start to adding cart storyboard

---
+++ Final-Project-by-Uladzslau-Kohan/Screens/Cart/Controller/CartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CartViewController.swift
+//  Final-Project-by-Uladzslau-Kohan
+//
+//  Created by VironIT on 8/25/22.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
